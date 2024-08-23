937eb626718743c711309995a8d3db9d19d9aba5
MuzammilCh <muzammilch617@gmail.com>
2024-08-23 16:34:53 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+      colors: {
+        'blue-ribbon': '#166FFF',
+        'l-gray': '#F2F7FF',
+      },
+
+    },
+  },
+  plugins: [],
+}
+
